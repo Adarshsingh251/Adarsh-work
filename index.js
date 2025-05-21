ae7c329d0bb6e8c83ffc7347fdbc1bed0178ae2f
Adarsh singh <adarshcomedy4@gmail.com>
2025-05-21 18:41:55 +0530

    added mobile version and more changes also been done

--- index.js
+++ index.js
@@ -6,5 +6,27 @@ var swiper = new Swiper(".slider",{
     speed: 2500,
     pagination: {
         el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true, //main zoom rest all small in size
+        // type: "progressbar", // linear bar animation
       },
+})
+
+var slider = new Swiper(".sliders",{
+    loop: true,
+    // autoplay: {
+    //     delay:2000
+    // },
+    slidesPerView: 8,
+    breakpoints:{
+        350:{
+            slidesPerView: 3,
+            spaceBetween : 18,
+        },
+        1300:{
+            slidesPerView: 8,
+            // spaceBetween : 18,
+        }
+    }
+
 })
