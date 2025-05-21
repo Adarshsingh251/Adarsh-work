dd743a9e5713e1bada9d6d60a0275c5dc3a8f1dd
Adarsh singh <adarshcomedy4@gmail.com>
2025-05-21 11:50:32 +0530

    added initial file

---
+++ index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".slider",{
+    loop: true,
+    autoplay: {
+        delay:2000
+    },
+    speed: 2500,
+    pagination: {
+        el: ".swiper-pagination",
+      },
+})
