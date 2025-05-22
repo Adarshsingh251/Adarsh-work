11d71a36630d8ebe9d283d065f2d6187dc2bceee
Adarsh singh <adarshcomedy4@gmail.com>
2025-05-22 17:54:56 +0530

    made full mobile reponsive

--- index.js
+++ index.js
@@ -30,3 +30,48 @@ var slider = new Swiper(".sliders",{
     }
 
 })
+
+var bottom = new Swiper(".bottom",{
+    loop: true,
+    slidesPerView: 8,
+    breakpoints:{
+        350:{
+            slidesPerView: 2,
+            spaceBetween : 3,
+        },
+        1300:{
+            slidesPerView: 8,
+            // spaceBetween : 18,
+        }
+    }
+})
+
+var beauty_bottom = new Swiper(".bottom_slide",{
+    loop: true,
+    slidesPerView: 8,
+    breakpoints:{
+        350:{
+            slidesPerView: 2,
+            spaceBetween : 3,
+        },
+        1300:{
+            slidesPerView: 8,
+            // spaceBetween : 18,
+        }
+    }
+})
+
+var sport = new Swiper(".sport_slider",{
+    loop: true,
+    slidesPerView: 8,
+    breakpoints:{
+        350:{
+            slidesPerView: 2,
+            spaceBetween : 3,
+        },
+        1300:{
+            slidesPerView: 8,
+            // spaceBetween : 18,
+        }
+    }
+})
