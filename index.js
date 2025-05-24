5884ba39d9a9ced81f28787c94f58c7fa9b5cc34
Adarsh singh <adarshcomedy4@gmail.com>
2025-05-24 22:47:10 +0530

    added login page

--- index.js
+++ index.js
@@ -1,77 +1,24 @@
-var swiper = new Swiper(".slider",{
-    loop: true,
-    autoplay: {
-        delay:2000
-    },
-    speed: 2500,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true, //main zoom rest all small in size
-        // type: "progressbar", // linear bar animation
-      },
+let login_containers = document.querySelector(".login_container");
+let login_btn = document.querySelector(".login");
+let cross = document.querySelector(".cross");
+let changed_login = document.querySelector(".changed_login")
+
+login_btn.addEventListener("click",function(){
+        login_containers.style.transform = "scale(1)";
 })
 
-var slider = new Swiper(".sliders",{
-    loop: true,
-    // autoplay: {
-    //     delay:2000
-    // },
-    slidesPerView: 8,
-    breakpoints:{
-        350:{
-            slidesPerView: 3,
-            spaceBetween : 18,
-        },
-        1300:{
-            slidesPerView: 8,
-            // spaceBetween : 18,
-        }
-    }
+cross.addEventListener("click",function(){
+    login_containers.style.transform = "scale(0)";
+})
+changed_login.addEventListener("click",function(){
+    event.preventDefault();
+    login_btn.innerHTML = "<h2>Sign Out</h2>";
+    login_containers.style.transform = "scale(0)";
 
 })
-
-var bottom = new Swiper(".bottom",{
-    loop: true,
-    slidesPerView: 5,
-    breakpoints:{
-        350:{
-            slidesPerView: 2,
-            spaceBetween : 3,
-        },
-        1300:{
-            slidesPerView: 5,
-            // spaceBetween : 18,
-        }
-    }
-})
-
-var beauty_bottom = new Swiper(".bottom_slide",{
-    loop: true,
-    slidesPerView: 5,
-    breakpoints:{
-        350:{
-            slidesPerView: 2,
-            spaceBetween : 3,
-        },
-        1300:{
-            slidesPerView: 5,
-            // spaceBetween : 18,
-        }
-    }
-})
-
-var sport = new Swiper(".sport_slider",{
-    loop: true,
-    slidesPerView: 5,
-    breakpoints:{
-        350:{
-            slidesPerView: 2,
-            spaceBetween : 3,
-        },
-        1300:{
-            slidesPerView: 5,
-            // spaceBetween : 18,
-        }
-    }
-})
+// login_btn.addEventListener('click', () => {
+//     login_containers.classList.add('active');
+// });
+// closeBtn.addEventListener('click', () => {
+//         login_containers.classList.remove('active');
+//     });
---
+++ swiper.js
@@ -0,0 +1,77 @@
+var swiper = new Swiper(".slider",{
+    loop: true,
+    autoplay: {
+        delay:2000
+    },
+    speed: 2500,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true, //main zoom rest all small in size
+        // type: "progressbar", // linear bar animation
+      },
+})
+
+var slider = new Swiper(".sliders",{
+    loop: true,
+    // autoplay: {
+    //     delay:2000
+    // },
+    slidesPerView: 8,
+    breakpoints:{
+        350:{
+            slidesPerView: 3,
+            spaceBetween : 18,
+        },
+        1300:{
+            slidesPerView: 8,
+            // spaceBetween : 18,
+        }
+    }
+
+})
+
+var bottom = new Swiper(".bottom",{
+    loop: true,
+    slidesPerView: 5,
+    breakpoints:{
+        350:{
+            slidesPerView: 2,
+            spaceBetween : 3,
+        },
+        1300:{
+            slidesPerView: 5,
+            // spaceBetween : 18,
+        }
+    }
+})
+
+var beauty_bottom = new Swiper(".bottom_slide",{
+    loop: true,
+    slidesPerView: 5,
+    breakpoints:{
+        350:{
+            slidesPerView: 2,
+            spaceBetween : 3,
+        },
+        1300:{
+            slidesPerView: 5,
+            // spaceBetween : 18,
+        }
+    }
+})
+
+var sport = new Swiper(".sport_slider",{
+    loop: true,
+    slidesPerView: 5,
+    breakpoints:{
+        350:{
+            slidesPerView: 2,
+            spaceBetween : 3,
+        },
+        1300:{
+            slidesPerView: 5,
+            // spaceBetween : 18,
+        }
+    }
+})
